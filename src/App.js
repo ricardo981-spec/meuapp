24942a26e957dcb0756b4c95e9411c090abf5b9a
ricardo981-spec <ricardocoutinholima009@gmail.com>
2023-10-25 11:18:19 -0300

    .

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello <code>Mundo</code>
+          Hello <code>World</code>
         </p>
         <a
           className="App-link"
